1434f86db278271b54423b9d84e4ea15ab46eb52
izadmehr <meysam.s@gmail.com>
2019-07-07 17:08:37 +0300

    Move error red color to constants

--- src/components/LoginForm/Styles.tsx
+++ src/components/LoginForm/Styles.tsx
@@ -58,7 +58,7 @@ export default styled.div`
         }
       }
       & > .error {
-        color: #d0011b;
+        color: ${colors.error};
         margin-top: 0.5em;
         border-radius: 3px;
         min-height: 1.25rem;
--- src/utlis/constants.ts
+++ src/utlis/constants.ts
@@ -1,4 +1,5 @@
 export const colors = {
   primary: "#007bff",
-  primaryDark: "#285f8f"
+  primaryDark: "#285f8f",
+  error: "#d0011b"
 };
